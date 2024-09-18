485ef12a740d663161bad936e0eac3cc4bb2a445
naushad1308 <naushad1308@gmail.com>
2024-09-18 13:05:43 +0530

    operators

---
+++ JavascriptTutes/datatype.js
@@ -0,0 +1,46 @@
+// let a = 'helllo'
+// console.log(a)
+
+
+// // console.log(abc)
+
+
+
+
+
+// let name = true
+// let age = 25
+// let elegible = false
+// let userDetails = {
+//     name: "abc",
+//     age: 25,
+//     elegible: true
+// }
+
+// let userName
+
+// // let usersName =  ['a','b',25, ]
+// // let usersName = ['a', 'b', 25, true]
+// const empDetails = null
+
+// // console.log(typeof (name))
+// // console.log(typeof (userDetails))
+// // console.log(typeof (userDetails))
+// // console.log(typeof (age))
+// // console.log(typeof (elegible))
+// // console.log(typeof (usersName))
+// // console.log(typeof (empDetails))
+
+
+// Arithmetic operations
+
+
+let a = 5
+let b = 4
+let c = a + b //9
+
+let x = b + c // 13
+b = x // b == 13
+
+b += c
+console.log(b)
---
+++ JavascriptTutes/script.js
@@ -0,0 +1,105 @@
+
+// - var, let, const are variable keywords
+// DATA TYPES - String,Number,boolean,object,null,undefined,arrray,bigInt
+
+// variables declaration
+
+// var name = "john"
+// let age = 25
+// const gender = "male"
+// console.log(name)
+// console.log(age)
+// console.log(gender)
+
+
+// data types
+// let name = "25" //string
+// let age = 30  //Number
+// let isMarried = true  //Boolean
+// let address = { street: "abc", city: "pqr", pin: 30042 } //object
+// let marks = null //null
+// let lastName = undefined //undefined
+// let numbers = [1, 2, 3, 4, 5] //array
+
+
+
+// operators
+//1 Arithmetic operator
+// + - * / %
+// let a = 11  // = Assignment Operator
+// let b = 2
+// console.log(a + b) //addition
+// console.log(a - b) //subtraction
+// console.log(a * b) //multiplication
+// console.log(a / b) //division
+// console.log(a % b) //modulus
+
+// 2 Unary Operators
+// ++, --, +, -, ~, !
+// let a = 10
+// let b = 2
+
+// a = a + b
+// a += b
+// console.log(a) //addition
+
+// Increment/Decrement Operator
+// let a = 5
+// let b = 2
+// a = a + 1
+// a++ // increment
+// a++ // increment
+// a-- // increment
+// a++ // increment
+
+// pre Increment/decrement - pahale value increment phir variable ko assign hogi
+
+//   Post Increment/decrement = pahle variable ko value assign hogi then value increase hogi
+
+// let d = a++
+// let d = ++a
+// console.log(d)
+// console.log(a) 
+
+
+// let a = 5
+// let b = 2
+// let d = ++a - b++
+
+
+// let x = a++ + ++a + ++b - --a + b++
+4
+// console.log(a) // 7
+// console.log(b) // 5
+// console.log(x) // 15
+
+// Comparision operator
+
+// ==, !=, ===, !==, >, <, >=, <=
+// let a = "hello"
+// let b = "hello"
+let a = 25
+let b = 12
+
+// console.log(a == x) // value
+// console.log(a === b) // value & data type
+// console.log(a >= b)
+// console.log(a <= b)
+// console.log(a == b)
+// console.log(a === b)
+// console.log(a !== b)
+
+// Logical Operators
+// &&  - AND operator 
+// ||- OR operator
+// !- - NOT operator
+
+let age1 = 25
+let age2 = 18
+console.log(age1 > 18 && age2 >= 18)
+// TRUE/1 && TRUE / 1   =  TRUE
+
+// console.log(age1 < 18 || age2 >= 18 && ag1 >= 25 || age2 < 18)
+console.log(false || true || true && false)
+
+// true
